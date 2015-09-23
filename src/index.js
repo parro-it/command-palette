167c95d1b2186a7812a71f004e447fbb7f2808e9
Andrea Parodi <a.parodi@inet2.it>
2015-09-23 17:42:29 +0200

    first release

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-module.exports = function test4() {
-  return Promise.resolve(42);
+'use strict';
+
+const Jets = require('jets');
+const fs = require('fs');
+const insertCss = require('insert-css');
+const css = fs.readFileSync(__dirname + '/style.css');
+insertCss(css);
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return ['', s4(), s4(), s4(), s4()].join('_');
+}
+
+exports.create = (commands) => {
+  const search = document.createElement('input');
+  const ul = document.createElement('ul');
+  const nav = document.createElement('nav');
+  nav.id = guid();
+  search.type = 'search';
+  nav.classList.add('command-palette');
+
+  commands.sort();
+  for (const cmd of commands) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(cmd));
+    ul.appendChild(li);
+  }
+
+  nav.appendChild(search);
+  nav.appendChild(ul);
+
+
+  setTimeout(() => {
+    const jet = new Jets({
+      searchTag: `#${nav.id} input`,
+      contentTag: `#${nav.id} ul`
+    });
+
+    search.addEventListener('input', () => {
+      const currentlyActive = document.querySelector(`#${nav.id} li.active`);
+      if (currentlyActive) {
+        currentlyActive.classList.remove('active');
+      }
+      const firstVisible = document.querySelector(
+        jet.styleTag.innerText
+          .slice(0, -15)
+          .replace(':not(', ' ')
+      );
+      if (firstVisible) {
+        firstVisible.classList.add('active');
+      }
+    });
+  });
+
+  return nav;
 };
--- test/index.js
+++ test/index.js
@@ -1,7 +1,20 @@
+'use strict';
+
 const test = require('tape-async');
 const commandPalette = require('..');
 
-test('add details files', function *(t) {
-  const result = yield commandPalette();
-  t.equal(result, 42);
+test('add details files', t => {
+  const results = commandPalette.create(Object.keys(document.body.style).reverse());
+  global.openWin = () => {
+    const BrowserWindow = require('remote').require('browser-window');
+    const win = BrowserWindow.getAllWindows()[0];
+
+    win.setSize(800, 600);
+    win.center();
+    win.show();
+  };
+
+  global.openWin();
+  document.body.appendChild(results);
+  console.log(results);
 });
